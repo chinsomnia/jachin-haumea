c19fff36f0fb1f54e17cb85a5e3bce243384e8ea
Jachin <152952438+chinsomnia@users.noreply.github.com>
2024-05-22 16:58:22 -0700

    added form and functionality

--- js/index.js
+++ js/index.js
@@ -38,3 +38,32 @@ fetch("https://api.github.com/users/chinsomnia/repos")
     }
   })
   .catch((error) => console.error(error));
+
+const messageForm = document.querySelector('[name="leave_message"]');
+
+messageForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const usersName = event.target.usersName.value;
+  const usersEmail = event.target.usersEmail.value;
+  const usersMessage = event.target.usersMessage.value;
+  console.log(usersName, usersEmail, usersMessage);
+
+  const messageSection = document.querySelector('[id="messages"]');
+  const messageList = messageSection.querySelector("ul");
+  const newMessage = document.createElement("li");
+  newMessage.innerHTML = `<a href="${usersEmail}">${usersName}</a>, <span>${usersMessage}</span>`;
+
+  //remove button
+  const removeButton = document.createElement("button");
+  removeButton.innerHTML = `<button>remove</button>`;
+  removeButton.addEventListener("click", (event) => {
+    const entry = event.target.parentNode;
+    entry.remove();
+    messageList.remove();
+  });
+  newMessage.appendChild(removeButton);
+  messageList.appendChild(newMessage);
+
+  //reset form
+  event.target.reset();
+});
